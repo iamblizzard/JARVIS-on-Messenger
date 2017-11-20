5736afea9ebd76932d79617bd3c7397ccfbe79d7
iamblizzard <nipunmittal15.com>
2017-11-20 23:03:11 +0530

    pro

---
+++ modules/src/__init__.py
@@ -0,0 +1,36 @@
+# List of all modules
+__all__ = [
+    'anime',
+    'book',
+    'bye',
+    'die',
+    'coin',
+    'currency',
+    'dice',
+    'dictionary',
+    'fact',
+    'hello',
+    'help',
+    'joke',
+    'lyrics',
+    'movie',
+    'music',
+    'news',
+    'ping',
+    'quote',
+    'request',
+    'thanks',
+    'time',
+    'url',
+    'video',
+    'weather',
+    'wiki',
+    'xkcd',
+]
+
+# List of modules that send data personalized to the user
+__personalized__ = [
+    'bye',
+    'hello',
+    'help',
+]
