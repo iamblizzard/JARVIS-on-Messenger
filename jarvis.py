2797968709b4f06bf6d2b417de8bcc74b2cd2ccc
iamblizzard <nipunmittal15.com>
2017-11-21 15:03:38 +0530

    pro

--- jarvis.py
+++ jarvis.py
@@ -118,6 +118,6 @@ def webhook():
 
 
 if __name__ == '__main__':
-	app.run()
+	#app.run()
 
-	#app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
